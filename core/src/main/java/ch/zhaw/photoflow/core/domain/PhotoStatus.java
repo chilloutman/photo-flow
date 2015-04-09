2c78aa879e446b2b4c0f2ad78b004d6b13590ba6
Lucas Neiva <lucasneiv4@gmail.com>
2015-04-09 18:07:15 +0200

    Added entries to the status enums.

--- core/src/main/java/ch/zhaw/photoflow/core/domain/PhotoStatus.java
+++ core/src/main/java/ch/zhaw/photoflow/core/domain/PhotoStatus.java
@@ -1,5 +1,18 @@
 package ch.zhaw.photoflow.core.domain;
 
 public enum PhotoStatus {
-
+	NEW("New"),
+	FLAGGED("Flagged"),
+	EDITING("Editing"),
+	DISCARDED("Discarded");
+	
+	private String name;
+	
+	private PhotoStatus(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
