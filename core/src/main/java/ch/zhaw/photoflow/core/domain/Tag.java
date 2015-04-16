c312194be71af432f2d0032b4712ee717f7fe3b7
joshfreak7 <joni.werren@gmail.com>
2015-04-16 10:33:05 +0200

    Attributes with getters and setters created.

--- core/src/main/java/ch/zhaw/photoflow/core/domain/Tag.java
+++ core/src/main/java/ch/zhaw/photoflow/core/domain/Tag.java
@@ -2,4 +2,19 @@ package ch.zhaw.photoflow.core.domain;
 
 public class Tag {
 
+	private String name;
+	
+	public Tag() {
+		
+	}
+
+	
+	/************ GETTERS AND SETTERS ************/
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
