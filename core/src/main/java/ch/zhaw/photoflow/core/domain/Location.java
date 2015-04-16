46c1c1d7ba0a4626cd5738774d818cd5f85ae8db
joshfreak7 <joni.werren@gmail.com>
2015-04-16 10:04:33 +0200

    Attributes and setters and getters.

--- core/src/main/java/ch/zhaw/photoflow/core/domain/Location.java
+++ core/src/main/java/ch/zhaw/photoflow/core/domain/Location.java
@@ -1,5 +1,27 @@
 package ch.zhaw.photoflow.core.domain;
 
 public class Location {
-
+	
+	private float lat;	//latitude
+	private float lon;	//longitude
+	
+	public Location() {
+		
+	}
+	
+	
+	/************ GETTERS AND SETTERS ************/
+	public float getLat() {
+		return lat;
+	}
+	public void setLat(float lat) {
+		this.lat = lat;
+	}
+	public float getLon() {
+		return lon;
+	}
+	public void setLon(float lon) {
+		this.lon = lon;
+	}
+	
 }
