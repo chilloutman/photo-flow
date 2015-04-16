c5cb44c5737fad55a9e35949fe303bb8d955b4ab
joshfreak7 <joni.werren@gmail.com>
2015-04-16 10:27:06 +0200

    Attributes with getters and setters added.

--- core/src/main/java/ch/zhaw/photoflow/core/domain/Photographer.java
+++ core/src/main/java/ch/zhaw/photoflow/core/domain/Photographer.java
@@ -2,4 +2,28 @@ package ch.zhaw.photoflow.core.domain;
 
 public class Photographer {
 
+	private String firstName;
+	private String lastName;
+	
+	public Photographer() {
+		
+	}
+
+	
+	/************ GETTERS AND SETTERS ************/
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
