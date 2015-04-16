ab5ac9c6d5dfeebd45552327326513dd17d61e2a
joshfreak7 <joni.werren@gmail.com>
2015-04-16 10:34:32 +0200

    Attributes with getters and setters created.

--- core/src/main/java/ch/zhaw/photoflow/core/domain/Todo.java
+++ core/src/main/java/ch/zhaw/photoflow/core/domain/Todo.java
@@ -1,5 +1,20 @@
 package ch.zhaw.photoflow.core.domain;
 
 public class Todo {
+	
+	private String description;
+	
+	public Todo() {
+		
+	}
 
+	
+	/************ GETTERS AND SETTERS ************/
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
