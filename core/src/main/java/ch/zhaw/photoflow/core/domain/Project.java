c3804c99657311a611d328982b17a36c1c76aaf7
joshfreak7 <joni.werren@gmail.com>
2015-04-16 10:31:42 +0200

    Attributes with getters and setters created.

--- core/src/main/java/ch/zhaw/photoflow/core/domain/Project.java
+++ core/src/main/java/ch/zhaw/photoflow/core/domain/Project.java
@@ -1,5 +1,49 @@
 package ch.zhaw.photoflow.core.domain;
 
+import java.util.List;
+
 public class Project {
 
+	private String name;
+	private String description;
+	private ProjectStatus status;
+	private List<Todo> todos;
+	
+	public Project() {
+		
+	}
+
+	
+	/************ GETTERS AND SETTERS ************/
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public ProjectStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(ProjectStatus status) {
+		this.status = status;
+	}
+
+	public List<Todo> getTodos() {
+		return todos;
+	}
+
+	public void setTodos(List<Todo> todos) {
+		this.todos = todos;
+	}
 }
