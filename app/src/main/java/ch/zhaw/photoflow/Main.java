6cbbde923bd72033279d7ef54991c49f7ce27a83
SimonGadgeteer <simon.schwarz@mail.com>
2015-04-19 17:16:27 +0200

    created main gui in JavaFx

--- app/src/main/java/ch/zhaw/photoflow/Main.java
+++ app/src/main/java/ch/zhaw/photoflow/Main.java
@@ -1,9 +1,37 @@
 package ch.zhaw.photoflow;
 
-public class Main {
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
 
   public static void main(String[] args) {
     System.out.println("hi");
+    
+    
+    launch(Main.class, args);
   }
+  
+  
+  @Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("view/main_gui.fxml"));
+          Scene scene = new Scene(page);
+          primaryStage.setScene(scene);
+          primaryStage.setTitle("Photo Flow");
+          primaryStage.show();
+      } catch (Exception ex) {
+          Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+      	System.out.println("I failed :-(");
+      }
+		
+	}
 
 }
---
+++ app/src/main/java/ch/zhaw/photoflow/ProjectController.java
@@ -0,0 +1,15 @@
+package ch.zhaw.photoflow;
+
+import javafx.fxml.FXML;
+
+
+public class ProjectController{
+	
+	
+	@FXML
+	private void changeTitle(javafx.event.ActionEvent event)
+	{
+		System.out.println("\nchanged title");
+	}
+
+}
