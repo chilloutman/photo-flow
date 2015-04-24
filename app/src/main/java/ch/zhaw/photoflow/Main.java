9ef667412cc6038efe417a8206378e4d2ebf0607
Lucas Neiva <lucasneiv4@gmail.com>
2015-04-24 13:14:55 +0200

    Instantiate PhotoFlow in Main.

--- app/src/main/java/ch/zhaw/photoflow/Main.java
+++ app/src/main/java/ch/zhaw/photoflow/Main.java
@@ -3,35 +3,34 @@ package ch.zhaw.photoflow;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import ch.zhaw.photoflow.core.PhotoFlow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
 
-  public static void main(String[] args) {
-    System.out.println("hi");
-    
-    
-    launch(Main.class, args);
-  }
-  
-  
-  @Override
+	public static void main(String[] args) {
+		launch(Main.class, args);
+	}
+
+	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
+			PhotoFlow photoFlow = new PhotoFlow();
+			// TODO Create the main controller here and inject *Dao and *Workflow objects.
 			AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("view/main_gui.fxml"));
-          Scene scene = new Scene(page);
-          primaryStage.setScene(scene);
-          primaryStage.setTitle("Photo Flow");
-          primaryStage.show();
-      } catch (Exception ex) {
-          Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-      	System.out.println("I failed :-(");
-      }
-		
+			Scene scene = new Scene(page);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Photo Flow");
+			primaryStage.show();
+		} catch (Exception ex) {
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+			System.out.println("I failed :-(");
+		}
+
 	}
 
 }
