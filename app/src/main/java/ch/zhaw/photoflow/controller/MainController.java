ad0b9ea81a2cd3f5d04a01b1a3a4ce0a42484d01
Lucas Neiva <lucasneiv4@gmail.com>
2015-05-05 19:50:32 +0200

    Use ListView<Project> instead of ListView<String> in MainController.
    Also, use ObservableList to avoid having to add and remove objects from
    the ListView.

--- app/src/main/java/ch/zhaw/photoflow/controller/MainController.java
+++ app/src/main/java/ch/zhaw/photoflow/controller/MainController.java
@@ -2,98 +2,120 @@ package ch.zhaw.photoflow.controller;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.stream.Collectors;
 
+import javafx.application.Platform;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.adapter.JavaBeanStringPropertyBuilder;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
+import javafx.scene.control.SelectionMode;
 import ch.zhaw.photoflow.core.DaoException;
 import ch.zhaw.photoflow.core.ProjectDao;
 import ch.zhaw.photoflow.core.domain.Project;
 import ch.zhaw.photoflow.core.domain.Tag;
 
+import com.google.common.annotations.VisibleForTesting;
+
 public class MainController extends AbstractController implements Initializable {
 
-	ProjectDao projectDao;
-	List<Project> projects;
-	Project project;
-	PopUpHandler popup;
-	Stage stage; 
-	Parent root;
+	/**
+	 * Special project that acts as the "add new project" button.
+	 */
+	private final Project ADD_NEW_PROJECT = Project.newProject(p -> p.setName("+ New Project"));
 	
-	//FMXL-stuff
+	private final ProjectDao projectDao;
+	private final ObservableList<Project> projects = FXCollections.observableArrayList();
+	private PopUpHandler popup;
+
 	@FXML
 	private ProjectController projectController;
-	
-	@FXML
-    private ListView<String> projectList;
 
-	
-	
+	@FXML
+	private ListView<Project> projectList;
+
 	private String projectName;
 	private String projectDescription;
 	private List<Tag> tags = new ArrayList<>();
-	
+
 	public MainController(ProjectDao projectDao) {
-		this.projects = new ArrayList<Project>();
 		this.projectDao = projectDao;
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
 		loadProjects();
+		projectList.setItems(projects);
+		projectList.setCellFactory(listView -> new ProjectCell());
+		projectList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		projectList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+			projectSelected(newValue);
+		});
 	}
 	
 	/**
 	 * Loads all the saved projects and adds them to the local list
 	 */
 	private void loadProjects() {
-		List<Project> tempProjects = new ArrayList<Project>(projects);
 		try {
 			this.projects.clear();
+			this.projects.add(ADD_NEW_PROJECT);
 			this.projects.addAll(projectDao.loadAll());
 		} catch (DaoException e) {
-			this.projects = tempProjects;
 		}
 	}
 	
+	public void projectSelected(Project selectedProject) {
+		if (selectedProject == ADD_NEW_PROJECT) {
+			// TODO: Could we not block the UI here and use a listener instead?
+			popup = new PopUpHandler();
+			Optional<Project> newProject = createProject();
+
+			// Update selection
+			Platform.runLater(() -> {
+				projectList.getSelectionModel().select(newProject.orElse(null));
+			});
+		} else {
+			//projectController.setProject(selectedProject);
+		}
+	}
+
 	/**
 	 * Processes stuff for object {@link Project} and adds to list.
 	 */
-	public void createProject() {
-		//todo tag handling
-		if(popup.getName() != null)
-		{
+	public Optional<Project> createProject() {
+		// todo tag handling
+		if (popup.getName() != null) {
 			setProjectName(popup.getName());
 			setProjectDescription(popup.getDesc());
 			tags = popup.getTags();
-			
-			System.out.println("Tags = "+tags);
-			
-			
-			project = Project.newProject(p -> {
+
+			System.out.println("Tags = " + tags);
+
+			Project project = Project.newProject(p -> {
 				p.setName(projectName);
 				p.setDescription(projectDescription);
 				p.setTags(tags);
 			});
 			addProject(project);
 			System.out.println("project created");
-		}
-		else
-		{
+			return Optional.of(project);
+		} else {
 			System.out.println("canceled project creation");
+			return Optional.empty();
 		}
-		
 	}
-	
+
 	/**
-	 * Adds a project using the ProjectDao. Also saves the project to the local list
-	 * @param project	the project to add
+	 * Adds a project using the ProjectDao. Also saves the project to the local
+	 * list
+	 * @param project the project to add
 	 */
 	public void addProject(Project project) {
 		try {
@@ -104,10 +126,10 @@ public class MainController extends AbstractController implements Initializable 
 			// TODO: Warn user
 		}
 	}
-	
+
 	/**
 	 * Deletes a project within the ProjectDao an the local list
-	 * @param project	the project to be deleted
+	 * @param project the project to be deleted
 	 */
 	public void deleteProject(Project project) {
 		try {
@@ -117,107 +139,39 @@ public class MainController extends AbstractController implements Initializable 
 			// TODO: Warn user
 		}
 	}
-	
-	/**
-	 * Updates projectList on the main_gui left side
-	 */
-	public void updateList()
-	{
-		//add "new Project" entry
-		List<String> np = Arrays.asList("+ new Project");
-		List<String> values = projects.stream().map((project)->{return project.getName();}).collect(Collectors.toList());
-		List<String> newList = new ArrayList<String>();
-		newList.addAll(np);
-		newList.addAll(values);
-		
-		projectList.setItems(FXCollections.observableList(newList));
-	}
-	
-	/*
-	 * Getter and Setter
-	 */
-	public List<Project> getProjects() {
-		return this.projects;
-	}
-	
-	public void setProjects(List<Project> projects) {
-		this.projects = projects;
-	}
 
-	public String getProjectName() {
-		return projectName;
+	@VisibleForTesting
+	protected List<Project> getProjects() {
+		return this.projects;
 	}
 
 	public void setProjectName(String projectName) {
 		this.projectName = projectName;
 	}
 
-	public String getProjectDescription() {
-		return projectDescription;
-	}
-
 	public void setProjectDescription(String projectDescription) {
 		this.projectDescription = projectDescription;
 	}
 
-	public List<Tag> getTags() {
-		return tags;
-	}
-
-	public void setTags(List<Tag> tags) {
-		this.tags = tags;
-	}
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		updateList();
-		//handle selecting by mouse or keyboard
-		projectList.setOnMouseClicked(this::handleMouseClick);
-		projectList.setOnKeyPressed(this::handleEnter);
-	}
-	
-	/**
-	 * Opens Project / Create new Project on Mouse selection  
-	 * @param arg0 Click Event from projectlist
-	 */
-	public void handleMouseClick(MouseEvent arg0) {
-	    System.out.println("clicked on " + projectList.getSelectionModel().getSelectedItem());
-	    
-	    if(projectList.getSelectionModel().getSelectedItem() == "+ new Project")
-	    {
-	    	popup = new PopUpHandler();
-	    	createProject();
-	    	updateList();
-	    }
-	    else
-	    {
-	    	//todo
-	    	//loadProject( projectList.getSelectionModel().getSelectedItem());
-	    }
-	}
-	
-	/**
-	 * Opens Project / Create new Project on Keyboard selection and Enter-Key pressed
-	 * @param arg0 Key Event from projectlist
-	 */
-	public void handleEnter(KeyEvent arg0) {
-		if(arg0.getCode() == KeyCode.ENTER)
-		{
-		    System.out.println("selected on " + projectList.getSelectionModel().getSelectedItem());
-		    
-		    if(projectList.getSelectionModel().getSelectedItem() == "+ new Project")
-		    {
-		    	popup = new PopUpHandler();
-		    	createProject();
-		    	updateList();
-		    }
-		    else
-		    {
-		    	//todo
-		    	//loadProject( projectList.getSelectionModel().getSelectedItem());
-		    }
+	private static final class ProjectCell extends ListCell<Project> {
+		
+		public ProjectCell() {
+			// We could probably avoid overriding updateItem below by somehow binding itemProperty() to project.getName() here.
+		}
+		
+		@Override
+		public void updateItem(Project project, boolean empty) {
+			super.updateItem(project, empty);
+			if (empty) return;
+			// Bind the text property to the projects name.
+			try {
+				textProperty().unbind();
+				StringProperty nameProperty = JavaBeanStringPropertyBuilder.create().bean(project).name("name").build();
+				textProperty().bind(nameProperty);
+			} catch (NoSuchMethodException e) {
+				throw new IllegalArgumentException(e);
+			}
 		}
 	}
-	
-	
+
 }
